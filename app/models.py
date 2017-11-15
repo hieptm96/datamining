88296baecfce66d8af707091e8b4fac8a00cc642
hieptm96 <hiep1996tb@gmail.com>
2017-11-15 19:52:00 +0700

    Add migration

--- app/models.py
+++ app/models.py
@@ -2,11 +2,25 @@ from django.db import models
 
 # Create your models here.
 class Question(models.Model):
-    question_text = models.CharField(max_length=200)
-    pub_date = models.DateTimeField('date published')
+  question_text = models.CharField(max_length=200)
+  pub_date = models.DateTimeField('date published')
 
 class Choice(models.Model):
-    question = models.ForeignKey(Question, on_delete=models.CASCADE)
-    choice_text = models.CharField(max_length=200)
-    votes = models.IntegerField(default=0)
+  question = models.ForeignKey(Question, on_delete=models.CASCADE)
+  choice_text = models.CharField(max_length=200)
+  votes = models.IntegerField(default=0)
 
+class News(models.Model):
+  title = models.TextField()
+  description = models.TextField()
+  url = models.TextField()
+  content = models.TextField()
+  time = models.DateTimeField('date published')
+
+class Cluster(models.Model):
+  cluster_core = models.IntegerField()
+  numOfDocuments = models.IntegerField()
+
+class DocumentRepresentation(models.Model):
+  representation = models.TextField()
+  cluster = models.ForeignKey(Cluster, on_delete=models.CASCADE)
