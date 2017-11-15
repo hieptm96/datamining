f01b7be1a919f0e012f1b87dfa93cd13acb140a0
hieptm96 <hiep1996tb@gmail.com>
2017-11-15 12:47:21 +0700

    Update

--- app/urls.py
+++ app/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import url
 
 from . import views
+from .controllers import testController
 
 urlpatterns = [
   # url /
   url(r'^$', views.index, name='index'),
   # url /test
-  url(r'^test', views.test, name='test'),
+  url(r'^test', testController.index, name='index'),
 ]
